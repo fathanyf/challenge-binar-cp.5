3239964ac78367c6a365e4f680506900db290232
Fathan Yuda F <fathanupn17@gmail.com>
2021-12-13 12:44:08 +0700

    menambah export module dan middileware

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 8000;
 const path = require('path');
 const userList = require("./user.json");
 
+const router = require("./router");
+app.use(router);
+
 let isAuthenticated = false;
 
 app.set('view-engine', 'ejs')
@@ -41,7 +44,9 @@ app.post('/login', (req, res) => {
     return res.redirect('/login');
 })
 
-
+app.get('/chapter-3', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public/chapter-3/homepage.html'));
+});
 app.get('/chapter-4', function(req, res) {
     res.sendFile(path.join(__dirname, 'public/chapter-4/index.html'));
 });
